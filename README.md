40d69649bc1a180243cdab40bd2eca047542c206
Tatuin <tatuin2010@hotmail.com>
2021-01-25 13:27:30 +0100

    Primer commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Servidor de Desarrollo Básico
+
+Servidor con todas las dependencias y configuraciones necesarias para empezar proyectos.
+
+## Caracteristicas
+##### Actualiza la ventana del navegador de manera automática.
+##### Importa estilos e imágenes de forma dinámica.
+##### Limpia la carpeta dist de manera automática.
+
+## Ejecuta el comando para instalar node_modules
+```
+npm run build
+```
---
+++ webpack.prod.js
@@ -0,0 +1,58 @@
+const path = require('path');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const MiniCssStylePlugin = require('mini-css-extract-plugin');
+const OptimiceCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MinifyPlugin = require('babel-minify-webpack-plugin');
+
+module.exports = {
+    mode: 'production',
+    optimization: {
+        minimizer: [new OptimiceCssAssetsPlugin()]
+    },
+    output: {
+        filename: 'main.[contenthash].js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    module: {
+        rules: [{
+                test: /\.css$/i,
+                exclude: /styles\.css$/i,
+                use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /styles\.css$/i,
+                use: [MiniCssStylePlugin.loader, 'css-loader']
+            },
+            {
+                test: /\.html$/i,
+                loader: 'html-loader',
+                options: { attributes: false, minimize: false },
+            },
+            {
+                test: /\.(png|svg|gif)$/,
+                use: [{ loader: 'file-loader', options: { esModule: false } }]
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: './src/index.html',
+            filename: './index.html'
+        }),
+        new MiniCssStylePlugin({
+            filename: '[name].[contenthash].css',
+            ignoreOrder: false
+        }),
+        new CopyPlugin({
+            patterns: [
+                { from: 'src/assets', to: 'assets/' },
+            ]
+        }),
+        new CleanWebpackPlugin(),
+        new MinifyPlugin(),
+    ],
+
+
+}
